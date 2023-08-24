75aaabfecb4bc864bc685ed06c26b55843aa4496
journeytd10 <journeytd10@gmail.com>
2023-08-24 12:43:57 -0400

    Initial Commit

---
+++ Study Buddy/Study_BuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Study_BuddyApp.swift
+//  Study Buddy
+//
+//  Created by Journey Galore on 8/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Study_BuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
